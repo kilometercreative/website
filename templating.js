c8264c96f43986b5ac69726282016c5426455dc8
kaedenwile <kaeden.wile@outlook.com>
2018-09-06 14:53:03 -0700

    Updated data handling

--- templating.js
+++ templating.js
@@ -1,46 +1,59 @@
+let data = [];
 
+function parseOnLoad(buildPage) {
+    let req = new XMLHttpRequest();
+    req.onreadystatechange = () => {
+        // do only if response object exists
+        if (req.readyState === 4 && req.response) {
+            let response = JSON.parse(req.response);
 
-function parseOnLoad() {
-    //empty for now because data is just a js object
+            // don't fail, just return null if cases aren't found
+            data = response.cases && response.cases.map((dataCase, index) => {
+                dataCase.id = index;
+                return dataCase;
+            });
 
-    //When the data has been received, set the content to animate in:
+            buildPage();
+        }
+    };
+    req.open("GET", "data.json");
+    req.send();
 }
 
 
 // **********
 // Temporary test functions to lay stuff out on page load
 
-function layoutWorkPreviewTest() {
+function layoutIndex(numberToShow) {
     let parent = document.getElementById('case-container');
-    for (i=0; i<data.cases.length; i++) {
-        console.log(i);
-        layoutWorkPreviewTile(parent, i, data.cases[i]);
+    for (let i = 0; i < numberToShow; i++) {
+        if (data[i].platform === "Web")
+            layoutWebTile(parent, data[i]);
+        else if (data[i].platform === "Mobile")
+            layoutAppTile(parent, data[i]);
     }
-}
-
-function layoutIndexTest() {
-    let parent = document.getElementById('case-container');
-    for (i=0; i<3; i++) {
-        data.cases[i].platform
-    }
-    layoutWebTile(parent, data.cases[0]);
-    layoutAppTile(parent, data.cases[1]);
 }
 
 function layoutProjectPageTest() {
     let hash = window.location.hash;
-    if (hash != "") {
-        let index = parseInt(hash.substr(1));
-        layoutProjectPage(index, data.cases[index]);
+    if (hash) {
+        let index = parseInt(hash.substr(1)); // cut off the "#"
+        layoutProjectPage(index, data[index]);
     } else {
         window.location.href = 'work.html';
+    }
+}
+
+function layoutWorkPreviewTest() {
+    let parent = document.getElementById('case-container');
+    for (let i = 0; i < data.length; i++) {
+        layoutWorkPreviewTile(parent, data[i]);
     }
 }
 
 // *********
 
 function layoutWebTile(parent, data) {
-
     parent.innerHTML +=
         "<div class='web-case'>" +
             "<img src='productImg/" + data.primaryImage + "' alt='' class='web-case-cover-img'/>" +
@@ -50,8 +63,8 @@ function layoutWebTile(parent, data) {
                     "<h4>"+ data.tech + "</h4>" +
                     "<h3>" + data.introduction + "</h3>" +
                     "<div class='links'>" +
-                        "<a href=data.link>Visit Site</a>" +
-                        "<a>View case study</a>" +
+                        "<a href='" + data.link + "'>Visit Site</a>" +
+                        "<a href='project.html#" + data.id + "'>View case study</a>" +
                     "</div>" +
                 "</div>" +
                 "<img src='productImg/" + data.secondaryImage + "' alt='' class='web-case-alt-img'/>" +
@@ -60,7 +73,6 @@ function layoutWebTile(parent, data) {
 }
 
 function layoutAppTile(parent, data) {
-
     parent.innerHTML +=
         "<div class='mobile-case'>" +
             "<div class='case-preview'>" +
@@ -69,8 +81,8 @@ function layoutAppTile(parent, data) {
                     "<h4>" + data.tech + "</h4>" +
                     "<h3>" + data.introduction + "</h3>" +
                     "<div class='links'>" +
-                        "<a>Download App</a>" +
-                        "<a>View case study</a>" +
+                        "<a>Download App</a>" + // TODO
+                        "<a href='project.html#" + data.id + "'>View case study</a>" +
                     "</div>" +
                 "</div>" +
                 "<img src='' alt='' class='mobile-case-cover-img'/>" +
@@ -78,25 +90,23 @@ function layoutAppTile(parent, data) {
         "</div>";
 }
 
-function layoutWorkPreviewTile(parent, index, data) {
-
+function layoutWorkPreviewTile(parent, data) {
     linkText = data.platform === "Mobile" ? "Download App" : "Visit Site";
-    projectLink = 'project.html#' + index;
+    projectLink = 'project.html#' + data.id;
     imageSource = 'productImg/' + data.previewImage;
-
-    console.log(imageSource);
 
     parent.innerHTML +=
         "<div class='default-case-preview'>" +
-            "<a href='" + projectLink + "' class='image-link'><img src='" + imageSource + "' alt='' class='default-case-preview-image'/></a>" +
+            "<a href='" + projectLink + "' class='image-link'>" +
+                "<img src='" + imageSource + "' alt='' class='default-case-preview-image'/>" +
+            "</a>" +
             "<h2>" + data.title + "</h2>" +
             "<h4>" + data.tech + "</h4>" +
-            "<a>" + linkText + "</a>" +
+            "<a href='" + data.link + "'>" + linkText + "</a>" +
         "</div>";
 }
 
 function layoutProjectPage(index, data) {
-
     // TODO - This opacity fade needs to be called once data is received, is funky rn
     document.getElementById('full-case-container').style.opacity = "1";
 
@@ -127,7 +137,7 @@ function layoutProjectPage(index, data) {
     }
 
     let caseBody = document.getElementById('project-image-container');
-    for (i=0; i<obj.productImages.length; i++) {
+    for (let i = 0; i < obj.productImages.length; i++) {
         caseBody.innerHTML += layoutProjectImageTile(obj.productImages[i]);
     }
 
